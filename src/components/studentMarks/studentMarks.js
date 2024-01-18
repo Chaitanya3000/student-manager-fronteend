9c824a191041e95deffe95f4808dd7b1f7120b8f
Chaitanya3000 <gurazalachaitanya@gmail.com>
2024-01-18 12:28:05 +0530

    adding json in gitignor

--- src/components/studentMarks/studentMarks.js
+++ src/components/studentMarks/studentMarks.js
@@ -10,22 +10,14 @@ function studentMarks() {
     <>
       <div className="thisPage">
         <div className="sidebar">
-          <Link to="/studentMarksGet" className="sideItems">
-            Get Marks
-          </Link>
+          <Link className="sideItems">Get Marks</Link>
           <Link className="sideItems">Update Marks</Link>
           <Link className="sideItems">Insert Marks</Link>
         </div>
         <div>
-          {one?(
-            <StudentMarksGet/>
-          ):(<></>)}
-          {two?(
-            <StudentMarksGet/>
-          ):(<></>)}
-          {three?(
-            <StudentMarksGet/>
-          ):(<></>)}
+          {one ? <StudentMarksGet /> : <></>}
+          {two ? <StudentMarksGet /> : <></>}
+          {three ? <StudentMarksGet /> : <></>}
         </div>
       </div>
     </>
--- src/components/studentMarks/studentMarksGet.js
+++ src/components/studentMarks/studentMarksGet.js
@@ -1,17 +1,21 @@
+import "./studentMarks.css";
+
 function studentMarksGet() {
   return (
     <>
-      <h1>Check Student Marks</h1>
-      <form>
-        <input className="" type="text" placeholder="Rollnumber" />
-        <select>
-          <option value="">-- Select --</option>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-        <input type="submit" />
-      </form>
+      <div className="marks">
+        <h1>Check Student Marks</h1>
+        <form>
+          <input className="formItems" type="text" placeholder="Rollnumber" />
+          <select className="formItems">
+            <option value="">-- Select --</option>
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
     </>
   );
 }
